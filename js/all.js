e24a02f25a3067344800a0df5e82b2e9c0d2578f
Icy <iwen722996@gmail.com>
2022-07-08 14:25:22 +0800

    7/8修改

--- js/all.js
+++ js/all.js
@@ -1,8 +1,51 @@
-var rellax = new Rellax('.rellax');
-
-$('.autoplay').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
+$(document).ready(function(){
+  $('.autoplay').slick({
+      dots: true,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 3000,
+      
+      responsive: [
+          {
+              breakpoint: 1024,
+              settings: {
+                  slidesToShow: 3,
+                  slidesToScroll: 3,
+                  infinite: true,
+                  dots: true
+              }
+          },
+          {
+              breakpoint: 768,
+              settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1
+                  
+              }
+          },
+          {
+              breakpoint: 576,
+              settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1
+              }
+          }
+      ]
   });
+});
+
+
+// $(function() {
+//   $("#bg_mountain_01").on(
+//     responsive: [
+//         {
+//             breakpoint: 768,
+//             $(".this").removeClass("active");
+//             e.preventDefault();
+//           }
+//             }
+//         },
+//       ]
+//     );
+// });
