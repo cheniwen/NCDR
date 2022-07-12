c1a14b6946ffd8cdb38881d603b8e2c625274949
Icy <iwen722996@gmail.com>
2022-07-12 13:41:04 +0800

    ˙7/12修改

--- js/all.js
+++ js/all.js
@@ -10,8 +10,8 @@ $(document).ready(function(){
           {
               breakpoint: 1024,
               settings: {
-                  slidesToShow: 3,
-                  slidesToScroll: 3,
+                  slidesToShow: 2,
+                  slidesToScroll: 1,
                   infinite: true,
                   dots: true
               }
